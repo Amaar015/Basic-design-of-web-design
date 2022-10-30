40ad9dff7ecf054bc5e1a06163ede272d6ebe604
amaar raza <razaammar582@gmail.com>
2022-10-30 18:56:01 +0500

    Javascript designe added

---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+var size=document.getElementById("btn1");
+var italiac=document.getElementById("btn2");
+var bold=document.getElementById("btn3");
+var under=document.getElementById("btn4");
+var center=document.getElementById("btn5");
+var left=document.getElementById("btn6");
+var right=document.getElementById("btn7");
+var bottom=document.getElementById("btn8");
+var top=document.getElementById("btn9");
+
+
